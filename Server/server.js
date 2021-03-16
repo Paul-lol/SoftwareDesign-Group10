26086374386d555625a15be36e830c605d6a63ff
Paul-lol <Paul-lol@users.noreply.github.com>
2021-03-16 16:47:42 -0500

    connected the server to the edit Profile Page

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,24 @@
-const express = require('express')
-const app = express(); 
 
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express(); 
 const PORT = process.env.PORT || 5000; 
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/edit", (req, res) => {
+  res.sendFile(path.join(__dirname, '../User', 'profile.html'));
+})
+
+app.post("/", (req,res) => {
+  res.send("Thanks for Posting that!");
+
+  console.log(req.body);
+})
+
+//For the server to load on the specified PORT 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+//Git
