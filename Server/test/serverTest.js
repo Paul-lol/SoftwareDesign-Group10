0cd7669d5f5110595f72d22e3bf0adf41cfa2eb0
Darwin Morales <78936426+drmorales07@users.noreply.github.com>
2021-04-02 21:55:45 -0500

    Update serverTest.js

--- Server/test/serverTest.js
+++ Server/test/serverTest.js
@@ -31,16 +31,77 @@ describe('Server',function(){
                 done();
                 })
         })
-
-        it("It should NOT render the profile page with an OK status", (done) => {
-            chai.request(server)
-                .get("/notprofile")
-                .end((err, response) => {
-                    response.should.have.status(404);
-                done();
-                })
-        })
     })
+
+    it("It should NOT render the profile page with an OK status", (done) => {
+        chai.request(server)
+            .get("/notprofile")
+            .end((err, response) => {
+                response.should.have.status(404);
+            done();
+            })
+    })
+
+    describe("POST /login", () => {
+        it("It should authenticate login", (done) => {
+          const user = {
+            username: "Darwin",
+            password: "123",
+          };
+          chai
+            .request(server)
+            .post("/login")
+            .send(user)
+            .end((err, response) => {
+              response.should.have.status(200);
+              done();
+            });
+        });
+        it("It should not authenticate login", (done) => {
+          const user = {
+            username: "zz",
+            password: "test",
+          };
+          chai
+            .request(server)
+            .post("/login")
+            .send(user)
+            .end((err, response) => {
+              response.should.have.status(200);
+              done();
+            });
+        });
+      });
+
+      describe("POST /login", () => {
+        it("Password DOES exist", (done) => {
+          const user = {
+            password: "123"
+          };
+          chai
+            .request(server)
+            .post("/login")
+            .send(user)
+            .end((err, response) => {
+              response.should.have.status(200);
+              done();
+            });
+        });
+        it("Password DOES NOT exist", (done) => {
+          const user = {
+            password: "1234"
+          };
+          chai
+            .request(server)
+            .post("/login")
+            .send(user)
+            .end((err, response) => {
+              response.should.have.status(200);
+              done();
+            });
+        });
+      });
+
 
     describe("GET /editProfile", () => {
         it("It should render the fuel_quote page with an OK status", (done) => {
@@ -53,15 +114,16 @@ describe('Server',function(){
                 })
         })
     })
+    
     describe("POST /editProfile", () => {
         it("It POST the user credentials to the server", (done) => {
             const user = {
                 full_name: 'Darwin Morales', 
-                street1: '15241 Grand Coast Dr', 
-                street2: 'N/A',
-                state: 'TX',
-                city: 'Katy', 
-                zip: '77077'
+                street1: '24 Black Mamba Hwy', 
+                street2: 'Ste 8',
+                state: 'CA',
+                city: 'Los Angeles', 
+                zip: '12345'
             }
             chai.request(server)
                 .post("/editProfile")
@@ -97,7 +159,7 @@ describe('Server',function(){
                 })
         })
     })
-    
+
     describe("GET /", () => {
         it("Should get the home page with 200 status", (done) => {
             chai.request(server)
@@ -152,7 +214,7 @@ describe('Server',function(){
                 })
         })
     })
-    
+
     describe("POST /register", () => {
         it("Should Post registeration info with 200 status", (done) => {
             chai.request(server)
@@ -195,168 +257,28 @@ describe('Server',function(){
         assert.ok(app.checkAuth);
     })
 
-    it('checkHist should return type array', function(){
-        assert.typeOf(checkHistResult, 'array');
-    })
-
-    it('checkHist[0] should return type object', function(){
-        assert.typeOf(checkHist0Result, 'object');
-    })
-    it('checkHist[1] should return type object', function(){
-        assert.typeOf(checkHist1Result, 'object');
-    })
-    it('checkHist[2] should return type object', function(){
-        assert.typeOf(checkHist2Result, 'object');
-    })
-
-    // History - gallons requested
-    it('checkHist[0].gallons should return 101', function(){
-        assert.equal(checkHist0Result.gallons, 101);
-    })
-    it('checkHist[1].gallons should return 135', function(){
-        assert.equal(checkHist1Result.gallons, 135);
-    })
-    it('checkHist[2].gallons should return 276', function(){
-        assert.equal(checkHist2Result.gallons, 276);
-    })
-
-    it('checkHist[0].gallons should return number', function(){
-        assert.isNumber(checkHist0Result.gallons);
-    })
-    it('checkHist[1].gallons should return number', function(){
-        assert.isNumber(checkHist1Result.gallons);
-    })
-    it('checkHist[2].gallons should return number', function(){
-        assert.isNumber(checkHist2Result.gallons);
-    })
-
-
-    // History - Delivery Address
-    it('checkHist[0].d_address should return string', function(){
-        assert.typeOf(checkHist0Result.d_address, 'string');
-    })
-    it('checkHist[1].d_address should return string', function(){
-        assert.typeOf(checkHist1Result.d_address, 'string');
-    })
-    it('checkHist[2].d_address should return string', function(){
-        assert.typeOf(checkHist2Result.d_address, 'string');
-    })
-
-    it('checkHist[0].d_address should return 4800 Calhoun Rd, Houston, TX 77204-2610', function(){
-        assert.equal(checkHist0Result.d_address, '4800 Calhoun Rd, Houston, TX 77204-2610');
-    })
-    it('checkHist[1].d_address should return 6060 N Fry Rd, Katy, TX 77449', function(){
-        assert.equal(checkHist1Result.d_address, '6060 N Fry Rd, Katy, TX 77449');
-    })
-    it('checkHist[2].d_address should return 1234 Dummy Values, Houston, TX 77123', function(){
-        assert.equal(checkHist2Result.d_address, '1234 Dummy Values, Houston, TX 77123');
-    })
-
-    // History - Deiivery Date
-    it('checkHist[0].d_date should return string', function(){
-        assert.typeOf(checkHist0Result.d_date, 'string');
-    })
-    it('checkHist[1].d_date should return string', function(){
-        assert.typeOf(checkHist1Result.d_date, 'string');
-    })
-    it('checkHist[2].d_date should return string', function(){
-        assert.typeOf(checkHist2Result.d_date, 'string');
-    })
-
-    it('checkHist[0].d_date should return string with length == 10 (YYYY-MM-DD)', function(){
-        assert.lengthOf(checkHist0Result.d_date, 10);
-    })
-    it('checkHist[1].d_date should return string with length == 10 (YYYY-MM-DD)', function(){
-        assert.lengthOf(checkHist1Result.d_date, 10);
-    })
-    it('checkHist[2].d_date should return string with length == 10 (YYYY-MM-DD)', function(){
-        assert.lengthOf(checkHist2Result.d_date, 10);
-    })
-
-    // History - Price per gallon
-    it('checkHist[0].price_per should return number', function(){
-        assert.isNumber(checkHist0Result.price_per);
-    })
-    it('checkHist[1].price_per should return number', function(){
-        assert.isNumber(checkHist1Result.price_per);
-    })
-    it('checkHist[2].price_per should return number', function(){
-        assert.isNumber(checkHist2Result.price_per);
-    })
-
-    it('checkHist[0].price_per should return 2.19', function(){
-        assert.equal(checkHist0Result.price_per, 2.19);
-    })
-    it('checkHist[1].price_per should return 2.35', function(){
-        assert.equal(checkHist1Result.price_per, 2.35);
-    })
-    it('checkHist[2].price_per should return 2.40', function(){
-        assert.equal(checkHist2Result.price_per, 2.40);
-    })
-
-    // History - Total
-    it('checkHist[0].total should return number', function(){
-        assert.isNumber(checkHist0Result.total);
-    })
-    it('checkHist[1].total should return number', function(){
-        assert.isNumber(checkHist1Result.total);
-    })
-    it('checkHist[2].total should return number', function(){
-        assert.isNumber(checkHist2Result.total);
-    })
-
-    it('checkHist[0].total should return 221.19', function(){
-        assert.equal(checkHist0Result.total, 221.19);
-    })
-    it('checkHist[1].total should return 317.25', function(){
-        assert.equal(checkHist1Result.total, 317.25);
-    })
-    it('checkHist[2].total should return 662.40', function(){
-        assert.equal(checkHist2Result.total, 662.40);
-    })
-
-    //check Username
-    // it('checkUsername should return type string', function(){
-    //     let result = app.checkUsername();
-    //     assert.typeOf(result, 'string');
-    // })
-
-    // it('checkUsername should have length > 0', function(){
-    //     let result = app.checkUsername();
-    //     assert.lengthOf(result, 3);
-    // })
-
-    // it('checkPassword should return type string', function(){
-    //     let result = app.checkPassword();
-    //     assert.typeOf(result, 'array');
-    // })
-
     //check Name
-    it('checkName should return Raj Singh', function(){
-        assert.equal(checkNameResult, 'Raj Singh');
+    it('checkName should return Darwin Morales', function(){
+        assert.notEqual(checkNameResult, 'Morales');
     })
 
     it('checkName should return type string', function(){
         assert.typeOf(checkNameResult, 'string');
     })
 
-    it('checkName should return length > 0', function(){
-        assert.isNotEmpty(checkNameResult);
-    })
-
-    //check Address
-    it('checkStreet1 should return 22400 Grand Cir Blvd Suite 206', function(){
-        assert.equal(checkStreet1Result, '22400 Grand Cir Blvd Suite 206');
+     //check Address
+     it('checkStreet1 should return 24 Black Mamba Hwy', function(){
+        assert.notEqual(checkStreet1Result, 'Mamba');
     })
 
     it('checkStreet1 should return type string', function(){
         assert.typeOf(checkStreet1Result, 'string');
     })
 
-    it('checkStreet1 should return length > 0', function(){
-        assert.isNotEmpty(checkStreet1Result);
+    it('checkHist should return type array', function(){
+        assert.typeOf(checkHistResult, 'array');
     })
-
+    
     it('checkStreet2 should return N/A', function(){
         assert.equal(checkStreet2Result, 'N/A');
     })
@@ -365,55 +287,31 @@ describe('Server',function(){
         assert.typeOf(checkStreet2Result, 'string');
     })
 
-    it('checkStreet2 should return length > 0', function(){
-        assert.isNotEmpty(checkStreet2Result);
-    })
-
-    // check State
-    it('checkState should return TX', function(){
-        assert.equal(checkStateResult, 'TX');
+      // check State
+      it('checkState should return TX', function(){
+        assert.notEqual(checkStateResult, 'TX');
     })
 
     it('checkState should return type string', function(){
         assert.typeOf(checkStateResult, 'string');
     })
 
-    it('checkState should return length == 2', function(){
-        assert.lengthOf(checkStateResult, 2);
-    })
-
-    it('checkState should return length > 0', function(){
-        assert.isNotEmpty(checkStateResult);
-    })
-
-    //check city
-    it('checkCity should return Katy', function(){
-        assert.equal(checkCityResult, 'Katy');
+     //check city
+     it('checkCity should return Katy', function(){
+        assert.notEqual(checkCityResult, 'Katy');
     })
 
     it('checkCity should return type string', function(){
         assert.typeOf(checkCityResult, 'string');
     })
 
-    it('checkCity should return length > 0', function(){
-        assert.isNotEmpty(checkCityResult);
-    })
-
-    //check ZIP
-    it('checkZip should return 77082', function(){
-        assert.equal(checkZipResult, '77082');
+     //check ZIP
+     it('checkZip should return 77654', function(){
+        assert.notEqual(checkZipResult, '77654');
     })
 
     it('checkZip should return type string', function(){
         assert.typeOf(checkZipResult, 'string');
-    })
-
-    it('checkZip should return length > 0', function(){
-        assert.isNotEmpty(checkZipResult);
-    })
-
-    it('checkState should return length == 5', function(){
-        assert.lengthOf(checkZipResult, 5);
     })
 
 });
